e80b79bf8eb402608eb7d679a693ebe3a6874ca9
traveIing <109046927+traveIing@users.noreply.github.com>
2023-07-31 21:46:27 -0700

    Update Progress Bar.lua

--- Progress Bar.lua
+++ Progress Bar.lua
@@ -1,61 +1,60 @@
+local LoadingBar = Instance.new("ScreenGui")
+local BackFrame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local BarProgress = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local LoadingText = Instance.new("TextLabel")
+LoadingBar.Name = "LoadingBar"
+LoadingBar.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+LoadingBar.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+BackFrame.Name = "BackFrame"
+BackFrame.Parent = LoadingBar
+BackFrame.BackgroundColor3 = Color3.fromRGB(65, 65, 65)
+BackFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+BackFrame.BorderSizePixel = 0
+BackFrame.Position = UDim2.new(0.5, -220, 1, 0)
+BackFrame.Size = UDim2.new(0, 441, 0, 57)
+UICorner.CornerRadius = UDim.new(0, 14)
+UICorner.Parent = BackFrame
+BarProgress.Name = "BarProgress"
+BarProgress.Parent = BackFrame
+BarProgress.BackgroundColor3 = Color3.fromRGB(107, 107, 107)
+BarProgress.BorderColor3 = Color3.fromRGB(0, 0, 0)
+BarProgress.BorderSizePixel = 0
+BarProgress.Size = UDim2.new(0, 0, 0, 57)
+UICorner_2.CornerRadius = UDim.new(0, 14)
+UICorner_2.Parent = BarProgress
+LoadingText.Name = "LoadingText"
+LoadingText.Parent = BackFrame
+LoadingText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+LoadingText.BackgroundTransparency = 1.000
+LoadingText.BorderColor3 = Color3.fromRGB(0, 0, 0)
+LoadingText.BorderSizePixel = 0
+LoadingText.Position = UDim2.new(0.274376422, 0, 0, 0)
+LoadingText.Size = UDim2.new(0, 200, 0, 57)
+LoadingText.Font = Enum.Font.SourceSans
+LoadingText.Text = "Loading..."
+LoadingText.TextColor3 = Color3.fromRGB(205, 210, 226)
+LoadingText.TextSize = 25.000
 local TweenService = game:GetService("TweenService")
-
-local function showBetaBar()
-    local screenGui = Instance.new("ScreenGui")
-    screenGui.Name = "LoadingScreenGui"
-    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-    local frame = Instance.new("Frame")
-    frame.Size = UDim2.new(0.3, 0, 0.05, 0)
-    frame.Position = UDim2.new(0.5, 0, 1, -frame.Size.Y.Offset - 10)
-    frame.AnchorPoint = Vector2.new(0.5, 1)
-    frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-    frame.BorderColor3 = Color3.fromRGB(128, 128, 128)
-    frame.BorderSizePixel = 2
-    frame.Parent = screenGui
-    frame.ClipsDescendants = true
-
-    local fillBar = Instance.new("Frame")
-    fillBar.Size = UDim2.new(0, 0, 1, 0)
-    fillBar.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-    fillBar.BorderSizePixel = 0
-    fillBar.Parent = frame
-
-    local progressText = Instance.new("TextLabel")
-    progressText.Size = UDim2.new(1, 0, 1, 0)
-    progressText.Position = UDim2.new(0, 0, 0, -5)
-    progressText.BackgroundTransparency = 1
-    progressText.Text = ""
-    progressText.TextColor3 = Color3.fromRGB(255, 255, 255)
-    progressText.Font = Enum.Font.SourceSans
-    progressText.TextSize = 18
-    progressText.Parent = frame
-
-    local function updateLoadingBar(currentProgress, totalProgress, labelText)
-        local percentage = currentProgress / totalProgress
-        fillBar:TweenSize(UDim2.new(percentage, 0, 1, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5, true)
-        progressText.Text = labelText or ("Loading: " .. math.floor(percentage * 100) .. "%")
-    end
-
-    local slideUpTween = TweenService:Create(frame, TweenInfo.new(1, Enum.EasingStyle.Quint), {Position = UDim2.new(0.5, 0, 0.95, -frame.Size.Y.Offset - 10)})
-    slideUpTween:Play()
-
-    local function setProgress(progress, labelText)
-        if progress >= 0 and progress <= 100 then
-            updateLoadingBar(progress, 100, labelText)
-            if progress >= 100 then
-                local slideDownTween = TweenService:Create(frame, TweenInfo.new(1, Enum.EasingStyle.Quint), {Position = UDim2.new(0.5, 0, 1.5, 0)})
-                slideDownTween:Play()
-                slideDownTween.Completed:Connect(function()
-                    screenGui:Destroy()
-                end)
-            end
-        else
-            warn("Invalid progress value. Progress should be between 0 and 100.")
+function setProgress(progress, labelText)
+    if progress >= 0 and progress <= 100 then
+        local percentage = progress / 100
+        local targetSize = UDim2.new(percentage, 0, 1, 0)
+        local tween = TweenService:Create(BarProgress, TweenInfo.new(0.5, Enum.EasingStyle.Quad), {Size = targetSize})
+        tween:Play()
+        LoadingText.Text = labelText or ("Loading: " .. math.floor(percentage * 100) .. "%")
+        if progress >= 100 then
+            task.wait(0.5)
+            local slideDownTween = TweenService:Create(BackFrame, TweenInfo.new(0.5, Enum.EasingStyle.Quad), {Position = UDim2.new(0.5, -220, 1.2, 0)})
+            slideDownTween:Play()
+            slideDownTween.Completed:Connect(function()
+                LoadingBar:Destroy()
+            end)
         end
+    else
+        warn("Invalid progress value. Progress should be between 0 and 100.")
     end
-
-    return setProgress
 end
-
-procBar = showBetaBar()
+local slideUpTween = TweenService:Create(BackFrame, TweenInfo.new(0.5, Enum.EasingStyle.Quad), {Position = UDim2.new(0.5, -220, 0.89, 0)})
+slideUpTween:Play()

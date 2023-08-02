2d106b264edeaee20fa1b8ef0a5b3c1b059ddf17
traveIing <109046927+traveIing@users.noreply.github.com>
2023-08-01 18:22:36 -0700

    :3

--- Command Bar.lua
+++ Command Bar.lua
@@ -1,67 +1,51 @@
-local BAR_WIDTH = 300
-local BAR_HEIGHT = 30
-local BAR_PADDING = 10
-local TOGGLE_KEY = Enum.KeyCode.RightBracket
+local CommandBar = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local BarTextBox = Instance.new("TextBox")
+local ToggleKey = Enum.KeyCode.Semicolon
 
-local gui2 = Instance.new("ScreenGui")
-gui2.Name = "CommandBar"
-gui2.Parent = game.CoreGui
+CommandBar.Name = "CommandBar"
+CommandBar.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+CommandBar.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-local frame = Instance.new("Frame")
-frame.Name = "Main"
-frame.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
-frame.BorderSizePixel = 0
-frame.Position = UDim2.new(1, -BAR_WIDTH - BAR_PADDING, 1, -BAR_HEIGHT - BAR_PADDING)
-frame.Size = UDim2.new(0, BAR_WIDTH, 0, BAR_HEIGHT)
-frame.Parent = gui2
+Main.Name = "Main"
+Main.Parent = CommandBar
+Main.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
+Main.BackgroundTransparency = 0.200
+Main.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.794447303, 0, 0.930463552, 0)
+Main.Size = UDim2.new(0, 292, 0, 42)
 
-local textBox = Instance.new("TextBox")
-textBox.Name = "Input"
-textBox.BackgroundTransparency = 1
-textBox.BorderSizePixel = 0
-textBox.ClearTextOnFocus = false
-textBox.Font = Enum.Font.SourceSans
-textBox.PlaceholderText = "Type a command... ']'"
-textBox.Position = UDim2.new(0, 5, 0, 5)
-textBox.Size = UDim2.new(1, -50, 1, -10)
-textBox.Text = ""
-textBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-textBox.TextSize = 18
-textBox.Parent = frame
+UICorner.Parent = Main
 
-local button = Instance.new("TextButton")
-button.Name = "Submit"
-button.BackgroundColor3 = Color3.fromRGB(0, 162, 232)
-button.BorderSizePixel = 0
-button.Position = UDim2.new(1, -40, 0, 5)
-button.Size = UDim2.new(0, 35, 1, -10)
-button.Font = Enum.Font.SourceSansBold
-button.Text = "SEND"
-button.TextColor3 = Color3.fromRGB(255, 255, 255)
-button.TextSize = 16
-button.AutoButtonColor = false
-button.Parent = frame
+BarTextBox.Name = "BarTextBox"
+BarTextBox.Parent = Main
+BarTextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BarTextBox.BackgroundTransparency = 1.000
+BarTextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+BarTextBox.BorderSizePixel = 0
+BarTextBox.Size = UDim2.new(0, 292, 0, 41)
+BarTextBox.Font = Enum.Font.SourceSans
+BarTextBox.PlaceholderText = "Type a command... ';'"
+BarTextBox.Text = ""
+BarTextBox.TextColor3 = Color3.fromRGB(160, 160, 160)
+BarTextBox.TextSize = 21.000
 
 local function onSubmit()
-    local input = textBox.Text
-    if input ~= "" then
-        if input:sub(1, 1) == "]" then
-            game.Players:Chat(input:sub(2))
-        else
-            game.Players:Chat(input)
-        end
-        textBox.Text = ""
+    local input = BarTextBox.Text
+    if (input ~= "") then
+        game.Players:Chat(input)
+        BarTextBox.Text = ""
     end
 end
-
-button.MouseButton1Click:Connect(onSubmit)
-textBox.FocusLost:Connect(onSubmit)
 
 local function onInput(input, gameProcessedEvent)
-    if not gameProcessedEvent and input.KeyCode == TOGGLE_KEY then
+    if (not gameProcessedEvent) and (input.KeyCode == ToggleKey) then
         task.wait(0.001)
-        textBox:CaptureFocus()
+        BarTextBox:CaptureFocus()
     end
 end
 
+BarTextBox.FocusLost:Connect(onSubmit)
 game:GetService("UserInputService").InputBegan:Connect(onInput)

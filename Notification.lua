1301b5cc83fcb51b4cf354fbb8b9df71308af697
traveIing <109046927+traveIing@users.noreply.github.com>
2023-09-06 09:36:07 -0700

    :3

--- Notification.lua
+++ Notification.lua
@@ -42,59 +42,9 @@ function notify(titletxt, text, time)
         GUI:Destroy()
     end)()
 end
-function notifyError(titletxt, text, time)
-    -- Error Sound
-    local Error = Instance.new("Sound")
-    Error.Name = "Error Sound"
-    Error.SoundId = "http://www.roblox.com/asset/?id=4841731967"
-    Error.Volume = 2
-    Error.Looped = false
-    Error.archivable = false
-    Error.Parent = game.Workspace
-    Error:Play()
-    -- Notification Code
-    local GUI = Instance.new("ScreenGui")
-    local Main = Instance.new("Frame", GUI)
-    local UICorner = Instance.new("UICorner", Main)
-    local title = Instance.new("TextLabel", Main)
-    local message = Instance.new("TextLabel", Main)
-    GUI.Name = "Notification"
-    GUI.Parent = game.CoreGui
-    Main.Name = "MainFrame"
-    Main.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
-    Main.BackgroundTransparency = 0.2
-    Main.BorderSizePixel = 0
-    Main.Position = UDim2.new(1, 330, 0, 50) -- Starting position outside the screen
-    Main.Size = UDim2.new(0, 330, 0, 100)
-
-    title.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
-    title.BackgroundTransparency = 0.9
-    title.Size = UDim2.new(1, 0, 0, 30)
-    title.Font = Enum.Font.SourceSansSemibold
-    title.Text = titletxt
-    title.TextColor3 = Color3.new(1, 1, 1)
-    title.TextSize = 20
-    title.TextWrapped = true
-
-    message.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
-    message.BackgroundTransparency = 1
-    message.Position = UDim2.new(0, 0, 0, 30)
-    message.Size = UDim2.new(1, 0, 1, -30)
-    message.Font = Enum.Font.SourceSans
-    message.Text = text
-    message.TextColor3 = Color3.new(1, 1, 1)
-    message.TextSize = 16
-    message.TextWrapped = true
-
-    -- Tween the Main frame in
-    Main:TweenPosition(UDim2.new(1, -330, 0, 50), "Out", "Sine", 0.5)
-
-    coroutine.wrap(function()
-        task.wait(time)
-        Main:TweenPosition(UDim2.new(1, 5, 0, 50), "Out", "Sine", 0.5)
-        task.wait(0.6)
-        GUI:Destroy()
-    end)()
+function notifyError(titletx, txt, time)
+    playAudio("Error")
+    notify(titletx, txt, time)
 end
 function chatNotify(reqMsg, reqColor)
     game.StarterGui:SetCore("ChatMakeSystemMessage", {

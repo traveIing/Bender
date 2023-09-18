f8a6540710887274d9e70459eff4869609482dd3
traveIing <109046927+traveIing@users.noreply.github.com>
2023-09-17 19:16:15 -0700

    :3

--- Map Changer.lua
+++ Map Changer.lua
@@ -144,7 +144,7 @@ local function prepMapChange()
     repeat task.wait() until game.Players.LocalPlayer.Backpack:FindFirstChild("PaintBucket")
     game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack.PaintBucket)
     repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("PaintBucket")
-    task.wait(0.5)
+    task.wait(1)
 end
 
 function ChangeMapColor(color)
